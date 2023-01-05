4cc86f38130ebd27d7aa5f854372b281da65e8d5
Patrick Riley <priley@relaytx.com>
2023-01-05 17:56:27 -0500

    Move code to search.py

--- tests/test_search.py
+++ tests/test_search.py
@@ -3,10 +3,14 @@
 import numpy as np
 import voxart
 
-def test_create_design_random():
+def test_search_design_random():
     rng = np.random.default_rng()
     def random_goal():
         return rng.choice(a=[0, 1], p=[0.7, 0.3], size=(7, 7))
     goal = voxart.Goal.from_arrays(
         random_goal(), random_goal(), random_goal())
-    design = voxart.create_design_random(goal)
+    goal.add_frame()
+    design = voxart.search_design_random(goal)
+    np.testing.assert_array_equal(goal.goal(0), design.projection(0))
+    np.testing.assert_array_equal(goal.goal(1), design.projection(1))
+    np.testing.assert_array_equal(goal.goal(2), design.projection(2))
