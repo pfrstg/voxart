bb53102b640aa1c171e06d1d956344843d22def7
Patrick Riley <priley@relaytx.com>
2023-01-05 19:34:13 -0500

    refactor Masks and testing

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,16 +1,98 @@
 # Copyright 2023, Patrick Riley, github: pfrstg
 
 import numpy as np
+import pytest
 import voxart
 
-def test_search_design_random():
-    rng = np.random.default_rng()
-    def random_goal():
+def test_masks():
+    masks = voxart.Masks(4)
+    np.testing.assert_array_equal(
+        masks.interior,
+        [[[False, False, False, False],
+          [False, False, False, False],
+          [False, False, False, False],
+          [False, False, False, False]],
+
+         [[False, False, False, False],
+          [False,  True,  True, False],
+          [False,  True,  True, False],
+          [False, False, False, False]],
+
+         [[False, False, False, False],
+          [False,  True,  True, False],
+          [False,  True,  True, False],
+          [False, False, False, False]],
+
+         [[False, False, False, False],
+          [False, False, False, False],
+          [False, False, False, False],
+          [False, False, False, False]]])
+
+    np.testing.assert_array_equal(
+        masks.faces,
+        [[[False, False, False, False],
+          [False,  True,  True, False],
+          [False,  True,  True, False],
+          [False, False, False, False]],
+
+         [[False,  True,  True, False],
+          [ True, False, False,  True],
+          [ True, False, False,  True],
+          [False,  True,  True, False]],
+
+         [[False,  True,  True, False],
+          [ True, False, False,  True],
+          [ True, False, False,  True],
+          [False,  True,  True, False]],
+
+         [[False, False, False, False],
+          [False,  True,  True, False],
+          [False,  True,  True, False],
+          [False, False, False, False]]])
+
+    np.testing.assert_array_equal(
+        masks.edges,
+        [[[ True,  True,  True,  True],
+          [ True, False, False,  True],
+          [ True, False, False,  True],
+          [ True,  True,  True,  True]],
+
+         [[ True, False, False,  True],
+          [False, False, False, False],
+          [False, False, False, False],
+          [ True, False, False,  True]],
+
+         [[ True, False, False,  True],
+          [False, False, False, False],
+          [False, False, False, False],
+          [ True, False, False,  True]],
+
+         [[ True,  True,  True,  True],
+          [ True, False, False,  True],
+          [ True, False, False,  True],
+          [ True,  True,  True,  True]]])
+
+@pytest.fixture
+def rng():
+    return np.random.default_rng()
+
+@pytest.fixture
+def random_goal(rng):
+    def one_view():
         return rng.choice(a=[0, 1], p=[0.7, 0.3], size=(7, 7))
     goal = voxart.Goal.from_arrays(
-        random_goal(), random_goal(), random_goal())
+        one_view(), one_view(), one_view())
     goal.add_frame()
-    design = voxart.search_design_random(goal)
-    np.testing.assert_array_equal(goal.goal(0), design.projection(0))
-    np.testing.assert_array_equal(goal.goal(1), design.projection(1))
-    np.testing.assert_array_equal(goal.goal(2), design.projection(2))
+    return goal
+
+def test_search_design_random(random_goal):
+    design = voxart.search_design_random(random_goal)
+    np.testing.assert_array_equal(random_goal.goal(0), design.projection(0))
+    np.testing.assert_array_equal(random_goal.goal(1), design.projection(1))
+    np.testing.assert_array_equal(random_goal.goal(2), design.projection(2))
+
+def test_search_design_random_face_first(random_goal):
+    design = voxart.search_design_random_face_first(random_goal)
+    np.testing.assert_array_equal(random_goal.goal(0), design.projection(0))
+    np.testing.assert_array_equal(random_goal.goal(1), design.projection(1))
+    np.testing.assert_array_equal(random_goal.goal(2), design.projection(2))
--- voxart/search.py
+++ voxart/search.py
@@ -10,32 +10,27 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def create_masks(size) -> Tuple[np.typing.NDArray, np.typing.NDArray, np.typing.NDArray]:
-    vox = np.zeros((size, size, size))
-    interior = np.full((size, size, size), False)
-    interior[1:size-1, 1:size-1, 1:size-1] = True
+class Masks:
+    def __init__(self, size: int):
+        self.interior = np.full((size, size, size), False)
+        self.interior[1:size-1, 1:size-1, 1:size-1] = True
 
-    edges = np.full((size, size, size), False)
-    for axis, idx0, idx1 in itertools.product(range(3), [0, size-1], [0, size-1]):
-        indices = [idx0, idx1]
-        indices.insert(axis, slice(None))
-        edges[tuple(indices)] = True
+        self.edges = np.full((size, size, size), False)
+        for axis, idx0, idx1 in itertools.product(range(3), [0, size-1], [0, size-1]):
+            indices = [idx0, idx1]
+            indices.insert(axis, slice(None))
+            self.edges[tuple(indices)] = True
 
-    faces = np.full((size, size, size), True) & ~interior & ~edges
-
-    return interior, faces, edges
+        self.faces = np.full((size, size, size), True) & ~self.interior & ~self.edges
 
 
-def search_design_random(goal, rng: Optional[np.random.Generator] = None) -> Design:
-    if rng is None:
-        rng = np.random.default_rng()
+def _random_search(design: Design, valid: np.typing.NDArray, rng: np.random.Generator):
+    """Performs a random search of removable pieces.
 
-    starting_design = goal.create_base_design()
-    interior, faces, edges = create_masks(goal.size)
-
-    design = copy.deepcopy(starting_design)
+    valid is a boolean array where True means the piece shoud lbe considered
+    """
     while True:
-        removable = design.find_removable_slow() & ~edges
+        removable = design.find_removable_slow() & valid
         if np.sum(removable) == 0:
             break
         removable_indices = np.where(removable)
@@ -45,8 +40,33 @@ def search_design_random(goal, rng: Optional[np.random.Generator] = None) -> Des
                 removable_indices[2][spot_idx])
         #print(f"Removing {spot}")
         design.vox[spot] = 0
-        assert np.all(design.projection(0) == starting_design.projection(0))
-        assert np.all(design.projection(1) == starting_design.projection(1))
-        assert np.all(design.projection(2) == starting_design.projection(2))
+
+
+def search_design_random(goal: Goal, rng: Optional[np.random.Generator] = None) -> Design:
+    if rng is None:
+        rng = np.random.default_rng()
+
+    starting_design = goal.create_base_design()
+    masks = Masks(goal.size)
+
+    design = copy.deepcopy(starting_design)
+    _random_search(design, ~masks.edges, rng)
 
     return design
+
+def search_design_random_face_first(
+        goal: Goal, rng: Optional[np.random.Generator] = None) -> Design:
+    if rng is None:
+        rng = np.random.default_rng()
+
+    starting_design = goal.create_base_design()
+    masks = Masks(goal.size)
+
+    design = copy.deepcopy(starting_design)
+    _random_search(design, masks.faces, rng)
+    _random_search(design, masks.interior, rng)
+
+    print("hi")
+    return design
+
+#def objective_value(design
